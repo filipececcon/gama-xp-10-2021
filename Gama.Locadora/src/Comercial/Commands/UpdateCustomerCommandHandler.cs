9f43afc65e382dc56187cc9a2b367facf3faa388
Filipe Ceccon de Alencar <filipececcon@gmail.com>
2021-10-26 16:58:38 -0300

    adicionado handler para alteração de cliente

--- Gama.Locadora/src/Comercial/Commands/UpdateCustomerCommandHandler.cs
+++ Gama.Locadora/src/Comercial/Commands/UpdateCustomerCommandHandler.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using Gama.Locadora.Comercial.Entities;
+using Gama.Locadora.Comercial.Requests;
+using Gama.Locadora.Shared.Handlers;
+using Gama.Locadora.Shared.Interfaces;
+using Gama.Locadora.Shared.ValueObjects;
+
 namespace Gama.Locadora.Comercial.Commands
 {
-    public class UpdateCustomerCommandHandler
+    public class UpdateCustomerCommandHandler : CommandHandler<Customer, UpdateCustomerRequest, Object>
     {
-        public UpdateCustomerCommandHandler()
+        public UpdateCustomerCommandHandler(ICommandRepository<Customer> repository) : base(repository)
         {
+        }
+
+        public override object Handle(UpdateCustomerRequest request)
+        {
+            var name = new Name() { FirstName = request.FirstName };
+
+            var cnh = new CNH(request.CNH);
+
+            var email = new Email() { Value = request.Email };
+
+            var dadosDoClienteParaAtualizar = new Customer(name, null, cnh, null, email);
+
+            Repository.Update(dadosDoClienteParaAtualizar, request.Id);
+
+            return null;
         }
     }
 }
--- Gama.Locadora/src/Data/Configs/CustomerConfiguration.cs
+++ Gama.Locadora/src/Data/Configs/CustomerConfiguration.cs
@@ -68,3 +68,5 @@ namespace Gama.Locadora.Infra.Data.Configs
 
     }
 }
+
+//FluentAPI
